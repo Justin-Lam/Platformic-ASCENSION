aa033f64d81815a4d10e3b771c67c2c770ae4712
Justin-Lam <justinflam9225k@gmail.com>
2024-09-22 16:51:02 -0700

    Pushing some changes that were never pushed and also trying to get the project language to become C#

--- Unity Version/Platformic ASCENSION/Assets/Scenes/Shop/ShopManagerEditor.cs
+++ Unity Version/Platformic ASCENSION/Assets/Scenes/Shop/ShopManagerEditor.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 
 [CustomEditor(typeof(ShopManager))]
@@ -19,3 +20,4 @@ public class ShopManagerEditor : Editor
 		DrawDefaultInspector();
 	}
 }
+#endif
